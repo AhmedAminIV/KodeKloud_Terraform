88238ab6fe96d2bb8b50a6647fac30ad50a4bf1e
AhmedAminIV <ahmedaminsamey@gmail.com>
2025-09-05 06:26:10 +0300

    Task 3  ✔️ - Level 1

--- Level 1/03-vpc/variables.tf
+++ Level 1/03-vpc/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  description = "AWS vpc CIDR range"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "AWS vpc tag Name"
+  type        = string
+  default     = "nautilus-vpc"
+}
