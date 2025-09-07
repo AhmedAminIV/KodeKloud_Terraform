71861524504807370abdaf6e7184db7ff378ae1e
AhmedAminIV <ahmedaminsamey@gmail.com>
2025-09-07 06:16:01 +0300

    Task 5  ✔️ - Level 1

--- Level 1/05-vpc-ipv6/variables.tf
+++ Level 1/05-vpc-ipv6/variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_name" {
+  description = "Vpc name tag"
+  type        = string
+  default     = "xfusion-vpc"
+}
+
+variable "cidr_block" {
+  description = "ipv4 cidr block range"
+  type        = string
+  default     = "10.0.0.0/24"
+}
