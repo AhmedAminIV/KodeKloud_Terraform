7442faed0922808be640602941bf70def69cc0b7
AhmedAminIV <ahmedaminsamey@gmail.com>
2025-09-08 06:09:40 +0300

    Task 6  ✔️ - Level 1

--- Level 1/06-elastic-ip/variables.tf
+++ Level 1/06-elastic-ip/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "eip_name" {
+  description = "Elastic ip name"
+  type        = string
+  default     = "devops-eip"
+}
