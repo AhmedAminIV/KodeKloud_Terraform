9c4be9a7fb72add5b6e0bbd6c9f9f65485ac8069
AhmedAminIV <ahmedaminsamey@gmail.com>
2025-09-09 06:54:00 +0300

    Task 7  ✔️ - Level 1

--- Level 1/07-ec2/outputs.tf
+++ Level 1/07-ec2/outputs.tf
@@ -1,1 +1,14 @@
-# TODO: Add outputs for ec2
+output "ec2_instance_id" {
+  description = "ID of the created EC2 instance"
+  value       = aws_instance.datacenter.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.datacenter.public_ip
+}
+
+output "private_key_path" {
+  description = "Path to the saved private key"
+  value       = local_file.private_key.filename
+}
