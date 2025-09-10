02aeb66fb4ff69c06e2dcdab7218effd69b90c0a
Ahmed Amin <nasummoner7@gmail.com>
2025-09-10 07:05:04 +0300

    Task 8  ✔️ - Level 1

--- Level 1/08-ami/description.md
+++ Level 1/08-ami/description.md
@@ -1,5 +1,10 @@
 # Task: Create an AMI from an EC2 instance using Terraform
 
+For this task, create an AMI from an existing EC2 instance named xfusion-ec2 using Terraform.
+Name of the AMI should be xfusion-ec2-ami, make sure AMI is in available state.
+The Terraform working directory is /home/bob/terraform.
+Update the main.tf file (do not create a separate .tf file) to create the AMI.
+
 This folder contains Terraform code to **Create an AMI from an EC2 instance using Terraform**.
 
 ## Files
--- Level 1/08-ami/variables.tf
+++ Level 1/08-ami/variables.tf
@@ -3,3 +3,27 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "Instance Name tag"
+  type        = string
+  default     = "xfusion-ec2"
+}
+
+variable "instance_ami" {
+  description = "AMI"
+  type        = string
+  default     = "ami-0c101f26f147fa7fd"
+}
+
+variable "ami_name" {
+  description = "Name of the AMI"
+  type        = string
+  default     = "xfusion-ec2-ami"
+}
