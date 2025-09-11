97028f5613aa856064db2fd210b737d9084c15f9
Ahmed Amin <nasummoner7@gmail.com>
2025-09-11 06:53:24 +0300

    Task 9  ✔️ - Level 1

--- Level 1/09-ebs-volume/outputs.tf
+++ Level 1/09-ebs-volume/outputs.tf
@@ -1,1 +1,9 @@
-# TODO: Add outputs for ebs-volume
+output "vol_id" {
+  description = "The id of the created EBS volume"
+  value = aws_ebs_volume.devops-volume.id
+}
+
+output "vol_az" {
+  description = "The id of the created EBS volume"
+  value = aws_ebs_volume.devops-volume.availability_zone
+}
--- Level 1/09-ebs-volume/variables.tf
+++ Level 1/09-ebs-volume/variables.tf
@@ -3,3 +3,21 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vol_name" {
+  description = "EBS Volume name"
+  type        = string
+  default     = "devops-volume"
+}
+
+variable "vol_type" {
+  description = "EBS Volume type"
+  type        = string
+  default     = "gp3"
+}
+
+variable "vol_size" {
+  description = "EBS Volume size (GiB)"
+  type        = number
+  default     = 2
+}
