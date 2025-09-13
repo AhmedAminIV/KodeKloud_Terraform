ce7bdc529a8df4e59d52d4802d916005bc535dd6
AhmedAminIV <ahmedaminsamey@gmail.com>
2025-09-13 06:26:13 +0300

    Task 11  ✔️ - Level 1

--- Level 1/11-alarm/variables.tf
+++ Level 1/11-alarm/variables.tf
@@ -3,3 +3,41 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+
+variable "alarm_name" {
+  description = "alarm name"
+  type        = string
+  default     = "datacenter-alarm"
+}
+
+variable "evaluation_period" {
+  description = "number of evaluation period"
+  type        = number
+  default     = 1
+}
+
+variable "period" {
+  description = "the single evaluation period in seconds"
+  type        = number
+  default     = 300
+}
+
+variable "metric_threshold" {
+  description = "the upper threshold percentage when to trigger the alarm"
+  type        = number
+  default     = 80
+}
+
+
+variable "metric_name" {
+  description = "which metric to observe"
+  type        = string
+  default     = "CPUUtilization"
+}
+
+variable "alarm_desc" {
+  description = "description for the alarm"
+  type        = string
+  default     = "This metric monitors ec2 cpu utilization"
+}
